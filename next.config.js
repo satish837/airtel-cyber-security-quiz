3fb733db7e6073084eb2d5f743251de9ab3dee62
Satish Kumar <kumar.satish837@gmail.com>
2025-10-08 11:23:57 +0530

    Fix Vercel deployment by removing problematic next.config.js and outputFileTracingRoot

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingRoot: '/Users/satish/Documents/circuslogic/airtel-cybersecurity-quiz'
-}
-
-module.exports = nextConfig
